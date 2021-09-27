96f5d87c9c7fef9eb6833243735dacfed3faa9b7
Delores VanHoose <lvanhoose5@gmail.com>
2021-09-27 17:21:16 -0500

    Update level0.js

--- scripts/levels/level0.js
+++ scripts/levels/level0.js
@@ -1,19 +1,22 @@
 demo.level0 = function(){};
 demo.level0.prototype = {
-    preload: function(){},
+    preload: function(){
+        
+    },
     create: function(){
         
         
     },
+    
     update: function(){
         
         // Collision
-        game.physics.arcade.collide(currentPlayer, levelZeroTiles);
+        //game.physics.arcade.collide(currentPlayer, levelZeroTiles);
 
         // Warping
-        game.physics.arcade.collide(currentPlayer, shop1, function(player, coin){spawn = 1; spawndirection = 1; changeLevel(0,"0_1");});
-        game.physics.arcade.collide(currentPlayer, shop2, function(player, coin){spawn = 2; spawndirection = -1; changeLevel(0,"0_2");});
-        updateMoney();
+        //game.physics.arcade.collide(currentPlayer, shop1, function(player, coin){spawn = 1; spawndirection = 1; changeLevel(0,"0_1");});
+        //game.physics.arcade.collide(currentPlayer, shop2, function(player, coin){spawn = 2; spawndirection = -1; changeLevel(0,"0_2");});
+        //
         
     }
 }
