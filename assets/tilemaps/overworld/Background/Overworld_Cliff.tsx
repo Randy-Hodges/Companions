a754e7edade480feb6785ad8cc7c19f882f8641f
Joshua Oh <65065560+multispark@users.noreply.github.com>
2021-10-04 17:00:39 -0500

    Adding Overworld map Tiles

---
+++ assets/tilemaps/overworld/Background/Overworld_Cliff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Overworld_cliff" tilewidth="16" tileheight="16" tilecount="63" columns="7">
+ <image source="../../../overworld_map/Ground/Cliff.png" width="112" height="144"/>
+</tileset>
---
+++ assets/tilemaps/overworld/Buildings/Overworld_RedTaverns.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Overworld_RedTaverns" tilewidth="16" tileheight="16" tilecount="12" columns="3">
+ <image source="../../../overworld_map/Buildings/Red/RedTaverns.png" width="48" height="64"/>
+</tileset>
