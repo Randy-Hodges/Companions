013e9c148dd8da1ae5a3e36bf28482e80e89659c
Joshua Oh <65065560+multispark@users.noreply.github.com>
2021-10-04 15:44:21 -0500

    Update to map

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 
-var gameWidth = 700, gameHeight = 350;
-var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, "");
-
-game.state.add('level0', demo.level0);
-game.state.add('level1', demo.level1);
-game.state.add('level1_1', demo.level1_1);
-game.state.add('level1_2', demo.level1_2);
-
+var gameWidth = 700, gameHeight = 480;
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS);
 
 // level select
 game.state.add("PlayGame", level_select.playGame);
 game.state.add("PlayLevel", level_select.playLevel);
 game.state.start("PlayGame");
+
+game.state.add('level0', demo.level0);
+game.state.add('level1', demo.level1);
+game.state.add('level1_1', demo.level1_1);
+game.state.add('level1_2', demo.level1_2);
