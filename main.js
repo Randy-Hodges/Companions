e183d118c23a2bf1fbf551db40dc097630f1b830
Joshua Oh <65065560+multispark@users.noreply.github.com>
2021-10-06 14:41:41 -0500

    Renaming

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ var gameWidth = 700, gameHeight = 480;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS);
 
 // level select
-game.state.add("PlayGame", level_select.playGame);
-game.state.add("PlayLevel", level_select.playLevel);
-game.state.start("PlayGame");
+game.state.add("Map", demo.levelSelect);
+game.state.start("Map");
 
 game.state.add('level0', demo.level0);
 game.state.add('level1', demo.level1);
---
+++ scripts/levels/levelSelect.js
@@ -0,0 +1,22 @@
+demo.levelSelect = function(){};
+demo.levelSelect.prototype = {
+    preload: function(){
+        
+    },
+    create: function(){
+        
+        
+    },
+    
+    update: function(){
+        
+        // Collision
+        //game.physics.arcade.collide(currentPlayer, levelZeroTiles);
+
+        // Warping
+        //game.physics.arcade.collide(currentPlayer, shop1, function(player, coin){spawn = 1; spawndirection = 1; changeLevel(0,"0_1");});
+        //game.physics.arcade.collide(currentPlayer, shop2, function(player, coin){spawn = 2; spawndirection = -1; changeLevel(0,"0_2");});
+        //
+        
+    }
+}
