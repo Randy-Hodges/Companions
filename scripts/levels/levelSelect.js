f2013762b729bfed1f05ed0517b108b3d2c97a51
Joshua Oh <65065560+multispark@users.noreply.github.com>
2021-10-06 15:05:35 -0500

    Importing Files

--- scripts/levels/levelSelect.js
+++ scripts/levels/levelSelect.js
@@ -1,8 +1,17 @@
 demo.levelSelect = function(){};
 demo.levelSelect.prototype = {
     preload: function(){
+        game.load.tilemap('overworldMap', "assets/tilemaps/overworld/overworld.json", null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('RedMarket', "assets\overworld_map\Buildings\Red\RedMarket.png");
+        game.load.image('RedTaverns', "assets\overworld_map\Buildings\Red\RedTaverns.png");
+        game.load.image('TexturedGrass', "assets\overworld_map\Ground\TexturedGrass.png");
+        game.load.image('Trees', "assets\overworld_map\Nature\Trees.png");
+        game.load.image('Cliff', "assets\overworld_map\Ground\Cliff.png");
+        game.load.image('AssortedGround', "assets\overworld_map\Ground\Grass.png");
         
+        game.load.audio('backtrack', "assets/audio/music/Blizzard Island.mp3");
     },
+
     create: function(){
         
         
