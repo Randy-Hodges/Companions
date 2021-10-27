1a884049a59cfb592db85e0db2363d51f4f7735e
Randy-Hodges <randyhodges0303@gmail.com>
2021-10-27 15:49:38 -0500

    created level unlock and level complete variables

---
+++ scripts/levels/levelCompletion.js
@@ -0,0 +1,17 @@
+level1Complete = false;
+level1Unlocked = true;
+
+level2Complete = false;
+level2Unlocked = false;
+
+level3Complete = false;
+level3Unlocked = false;
+
+level4Complete = false;
+level4Unlocked = false;
+
+level5Complete = false;
+level5Unlocked = false;
+
+level6Complete = false;
+level6Unlocked = false;
